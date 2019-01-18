ccf5356c127e734d99ede99ce5bed57c84fc8069
Lion <extra.lion.w@gmail.com>
2019-01-18 15:20:00 +0100

    Start working on custom display math

--- panscola/mathfilter.py
+++ panscola/mathfilter.py
@@ -10,19 +10,42 @@ import re
 
 from panscola import utils
 
-aligned = re.compile(r'^\\begin{aligned}|\\end{aligned}$')
+aligned = re.compile(r"^\\begin{aligned}|\\end{aligned}$")
+
+
+class MyMath(pf.Math):
+    __slots__ = ["identifier", "classes", "attributes"]
+
+    def __init__(
+        self,
+        text,
+        format="DisplayMath",
+        identifier="",
+        classes=[],
+        attributes={},
+    ):
+        super().__init__(text, format)
+        self._set_ica(identifier, classes, attributes)
+
+
+@utils.make_dependent()
+def custom_display_math(elem, doc):
+    if isinstance(elem, pf.Math) and elem.format == "Displaymath":
+        new_elem = MyMath(elem.text, elem.format)
+        return new_elem
+
 
 @utils.make_dependent()
 def math(elem, doc):
     if (
         isinstance(elem, pf.Math)
-        and elem.format == 'DisplayMath'
-        and doc.format == 'latex'
+        and elem.format == "DisplayMath"
+        and doc.format == "latex"
     ):
 
-        text = aligned.sub('', elem.text)
-        if text.strip().startswith('\\begin'):
-            return pf.RawInline(text, format='latex')
+        text = aligned.sub("", elem.text)
+        if text.strip().startswith("\\begin"):
+            return pf.RawInline(text, format="latex")
         else:
             return None
 
@@ -39,14 +62,12 @@ def _finalize(doc):
 
 def main(doc=None):
     pf.run_filters(
-        utils.reduce_dependencies(
-            math,
-        ),
+        utils.reduce_dependencies(math),
         finalize=_finalize.to_function(),
         prepare=_prepare.to_function(),
         doc=doc,
     )
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
